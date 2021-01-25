b63d56e632027a5cbe97f6f03d8f4ad33d6ec05d
Janderson Souza <jandersonsouzagit@gmail.com>
2021-01-25 17:23:03 -0300

    Iniciando

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,9 +7,17 @@
 </head>
 <body>
     <div id="pagina">
-        <div>Logo</div>
-        <div>Conteudo</div>
-        <div>Rodape</div>
+        <div id="top">
+            <div id="logo">
+                Logo
+            </div>
+        </div>
+        <div id="conteudo">
+            Conteudo
+        </div>
+        <div id="rodape">
+            Rodape
+        </div>
     </div>
 </body>
 </html>
