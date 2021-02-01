8fa77517546823ee6d71c956b763cc859e93bb3d
Janderson Souza <jandersonsouzagit@gmail.com>
2021-02-01 17:31:35 -0300

    Dia 7. Att 3

--- exibe.php
+++ exibe.php
@@ -16,25 +16,38 @@
                 <div id="conteudo">
                     <div id="funcionalidade2">
                         <div id="exibe">
+                            <?php
+                                $conectar = mysqli_connect("localhost","root","","vitrine_php");
+                                $cod = $_GET["cod"];
+                                $slq_consulta = "SELECT * from produtos
+                                        WHERE cod_prod = '$cod'";
+
+                                $reult = mysqli_query($conectar,$slq_consulta);
+
+                                $dados_exibe = mysqli_fetch_row($reult);
+                            ?>
                             <div id="imagem">
-                                <img src="img/teste.jpg" alt="Produto" width="100%" height="70%" align="middle">
+                                <img src="<?php echo "$dados_exibe[5]"; ?>" alt="Produto">
                             </div>
                             <a href="index.php"><button><span>Voltar</span></button></a>
                             <div id="informes">
                                 <div id="texto">
                                     <ul>
-                                        <li>Titulo</li>
-                                        <li>de:  <span>  500$ </span> </li>
-                                        <li>Por: <span>  450$ </span> </li>
-                                        <li> ou <span> 12x </span> de <span> 41,66$</span> sem juros</li>
-                                        <li> à vista no boleto: <span>400$</span> </li>
+                                        <li><?php echo "$dados_exibe[1]"." $dados_exibe[2]";?></li>
+                                        <li>de:  <span>  <?php echo number_format($dados_exibe[3], 2, ',', '.');?>$ </span> </li>
+                                        <?php  $preco = ($dados_exibe[3] - (5/100 * $dados_exibe[3]))?>
+                                        <li>Por: <span><?php echo number_format($preco, 2, ',', '.');?>$ </span> </li>
+                                        <?php  $cartao = $preco /12?>
+                                        <li> ou <span> 12x </span> de <span> <?php echo number_format($cartao, 2, ',', '.');?>$</span> sem juros</li>
+                                        <?php  $preco2 = ($dados_exibe[3] - (10/100 * $dados_exibe[3]))?>
+                                        <li> à vista no boleto: <span><?php echo number_format($preco2, 2, ',', '.');?>$</span> </li>
                                     </ul>
                                 </div>
                             </div>
                             <div id="descricao">
-                                <div id="texto_descricao" style='text-align:center'">
+                                <div id="texto_descricao" style='text-align:center'>
                                     <h3>Descrição</h3>
-                                    <p>O Lenovo Ideapad S145 tem um design sofisticado leve e compacto. Possui tela antirreflexo de 15.6” para mais conforto visual e definição de imagem. Um notebook perfeito para produtividade e entretenimento com Wireless AC ultrarrápido e teclado numérico.</p>
+                                    <p><?php echo "$dados_exibe[4]";?></p>
                                 </div>
                             </div>
                         </div>
--- index.php
+++ index.php
@@ -18,9 +18,29 @@
                         <h1>Modelos</h1>
                         <ul>
                             <?php
-                            for($a=1; $a <= 5; $a++){
+                            
+                            $conectar = mysqli_connect("localhost","root","","vitrine_php");
+
+                            $sql_qtd = "SELECT  COUNT( marca)
+                                                      FROM produtos
+                                                      GROUP BY marca
+                                                      HAVING (COUNT(*) > 0)
+                                                      ";
+
+                            $sql_consulta_marca = "SELECT marca, COUNT(marca)
+                                        FROM produtos
+                                        GROUP BY marca
+                                        HAVING (COUNT(*) > 0)
+                                        ";
+
+                            $result_qtd = mysqli_query ($conectar, $sql_qtd);
+
+                            $resultado_marca = mysqli_query ($conectar, $sql_consulta_marca);
+
+                            while($consulta = mysqli_fetch_row($result_qtd)){
+                                $consulta_marca = mysqli_fetch_row($resultado_marca);
                             ?>
-                                <li><a href="index.php?cod = <?php echo $a;?>"><?php echo "Marca-($a)"; ?></a></li>
+                                <li><a href="index.php?marca=<?php echo "$consulta_marca[0]";?>"><?php echo "$consulta_marca[0]"."-($consulta[0])"; ?></a></li>
                             <?php
                             }
                             ?>
@@ -28,21 +48,30 @@
                     </div>
                     <div id="funcionalidade">
                         <div id="conteiner">
-                            <?php for($i=1; $i <= 5; $i++){?>
-                                <a href="exibe.php?cod = <?php echo $i ?>">
+                            <?php
+
+                                $slq_consulta = "SELECT * from produtos";
+
+                                $reult = mysqli_query($conectar,$slq_consulta);
+
+                                while($dados = mysqli_fetch_row($reult)){?>
+                                
                                 <div id="conteudo_especifico">
                                     <div id="central">
                                         <div id="imagem1">
-                                        <a href="exibe.php?cod = <?php echo $i ?>"><img  width="85%" height="85%" src="img/teste.jpg" alt="Produto"></a>
+                                        <a href="exibe.php?cod=<?php echo "$dados[0]";?>"><img  width="85%" height="85%" src="<?php echo "$dados[5]"; ?>" alt="Produto"></a>
                                         </div>
                                     </div>
                                     <div id="detalhes">
                                         <ul>
-                                            <li>Titulo</li>
-                                            <li>de:  <span>  500$ </span> </li>
-                                            <li>Por: <span>  450$ </span> </li>
-                                            <li> ou <span> 12x </span> de <span> 41,66$</span> sem juros</li>
-                                            <li> à vista no boleto: <span>400$</span> </li>
+                                            <li><?php echo "$dados[1]"." $dados[2]";?></li>
+                                            <li>de:  <span>  <?php echo number_format($dados[3], 2, ',', '.');?>$ </span> </li>
+                                            <?php  $preco = ($dados[3] - (5/100 * $dados[3]))?>
+                                            <li>Por: <span><?php echo number_format($preco, 2, ',', '.');?>$ </span> </li>
+                                            <?php  $cartao = $preco /12?>
+                                            <li> ou <span> 12x </span> de <span> <?php echo number_format($cartao, 2, ',', '.');?>$</span> sem juros</li>
+                                            <?php  $preco2 = ($dados[3] - (10/100 * $dados[3]))?>
+                                            <li> à vista no boleto: <span><?php echo number_format($preco2, 2, ',', '.');?>$</span> </li>
                                         </ul>
                                     </div>
                                 </div>
