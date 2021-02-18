04d3d6040807f83023653846f0e1ab8168ad7441
Janderson Souza <jandersonsouzagit@gmail.com>
2021-02-18 16:22:32 -0300

    Administracao

---
+++ administracao.php
@@ -0,0 +1,67 @@
+<?php
+   
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="css/fotmata.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+        <script defer src="js/div-hidden-show.js"></script>
+    </head>
+    <body>
+        <div id="top">
+            <a href="administracao.php">
+                <div id="logo">
+                    <h1 class="texto_logo">PHP</h1>
+                    <p class="texto_logo_p">Vitrine</p>
+                </div>
+            </a>
+            <div id="pesquisar">
+                <div id="faz_pesquisa">
+                    <p>Administração</p>
+                </div>
+            </div>
+            <div id="conteiner_interacao">
+                <div id="carrinho">
+                    <div id="texto_carro">
+                    </div>
+                </div>
+                <div id="login">
+                    <button  id="btn_teste" type="button"><i class="far fa-user-circle"></i></button>
+                    <div id="dados1" class="dados">
+                        <?php include "menu.php";?>
+                    </div>
+                </div>
+                <div id="boasVindas">
+                        <?php include "boas_vindas.php";?>
+                </div>
+            </div>
+        </div>
+        <div id="conteudo">
+            <div id="div_administracao">
+                <div id="menu_adm">
+                    <h1>Administração</h1>
+                    <ul>
+                        <li><a href="#">Vendas</a></li>
+                        <li><a href="#">Estoque</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="div_administracao">
+                <div id="menu_adm">
+                    <h1>Administração</h1>
+                    <ul>
+                        <li><a href="#">Vendas</a></li>
+                        <li><a href="#">Estoque</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div id="rodape">
+            <?php include "rodape.php";?>
+        </div>
+    </body>
+</html>
--- processa_login.php
+++ processa_login.php
@@ -1,44 +1,55 @@
 <?php
 	session_start();
 
-	/* conexÃ£o com o banco de dados */
 	$conecta = mysqli_connect ("localhost", "root", "", "php_vitrine");
 	
 	$login = $_POST["login"];
 	$senha = $_POST["senha"];	
-
-	/* realiza a consulta no banco de dados */	
+	
 	$sql_consulta = "SELECT  usuarios_cod_user,email, senha FROM logins
 					 WHERE 
 					       email = '$login' 
 					 AND 
 						   senha = '$senha'";
-						   
-	/* armazena  o resultado da consulta do banco de dados */				 
+			 
 	$result_consulta = mysqli_query($conecta, $sql_consulta);
 
-	/* retorna um valor caso encontre as informaÃ§Ãµes no banco de dados */
 	$linhas = mysqli_num_rows($result_consulta);
-	
-	/* faz a verificaÃ§Ã£o do retorno do banco de dados 
-	e redireciona para proxima pagina caso estaja todo certo ou emite uma menssagem 
-	caso os dados nÃ£o estejam no banco  */
 
 	if ($linhas == 1) {	
 		$dados = mysqli_fetch_row($result_consulta);
 
         $_SESSION["usuarios_cod_user"] = $dados[0];
 		$_SESSION["email"] = $dados[1];
-		$_SESSION["senha"] = $dados[2];	
+		$_SESSION["senha"] = $dados[2];
 		
-        echo "<script> 
+		$sql_status_user = "SELECT status_user FROM usuarios
+									WHERE cod_user = '$dados[0]'";
+		
+        $result_sql_status_user = mysqli_query($conecta,$sql_status_user);
+
+		$status_user = mysqli_fetch_row($result_sql_status_user);
+
+		if($status_user[0] == "administrador"){
+
+			echo "<script> 
+				  alert ('Bem-Vindo Administrador!!') 
+			  	</script>";
+			echo "<script> 
+						location.href = ('administracao.php')
+				</script>";
+		}else{
+
+			echo "<script> 
 				  alert ('Bem-Vindo!!') 
-			  </script>";
-		echo "<script> 
-					location.href = ('index.php')
-			  </script>";
+			  	</script>";
+			echo "<script> 
+						location.href = ('index.php')
+				</script>";
+		}
 	}
 	else {
+
 		echo "<script> 
 				  alert ('Usuario ou Senha Incorretos! Digite Novamente!!') 
 			  </script>";
